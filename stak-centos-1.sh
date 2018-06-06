fb53a74cf8cd393fd359389d5c72d0e7e5a405cc
Satoshi Nakamoto <34924479+MrNakamoto@users.noreply.github.com>
2018-06-06 15:58:03 -0700

    Update stak-centos-1.sh

--- stak-centos-1.sh
+++ stak-centos-1.sh
@@ -3,11 +3,11 @@ sudo yum -y groupinstall "Development Tools"
 sudo yum install -y cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make vixie-cron crontabs
 echo 'source /opt/rh/devtoolset-4/enable' >> ~/.bashrc
 source ~/.bashrc
-git clone https://github.com/MrNakamoto/xmr-stak-triton.git && mkdir xmr-stak/build
-cd xmr-stak/build && cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
+git clone https://github.com/MrNakamoto/xmr-stak.git && mkdir xmr-stak/build
+cd xmr-stak-triton/build && cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make install
 cd bin
-mv ~/stak/expect-2.sh ~/stak/xmr-stak/build/bin
+mv ~/xmr-stak-triton/expect-2.sh ~/stak/xmr-stak/build/bin
 expect ./expect-2.sh
 tmux
 trap "" 15
